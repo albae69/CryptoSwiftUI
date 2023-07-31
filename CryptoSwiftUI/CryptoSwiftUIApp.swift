e6080380b2a10230fabf63c9a3e2887405c3001c
albae69 <ahmadalbaihaqi69@gmail.com>
2023-07-31 13:26:03 +0700

    Initial Commit

---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by co2 on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
